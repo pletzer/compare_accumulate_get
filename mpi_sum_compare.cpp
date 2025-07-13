633d86ef69f447c9f8e202fff29e148f7bbb42d0
Alex Pletzer <alexander@gokliya.net>
2025-07-14 10:21:14 +1200

    working case

--- mpi_sum_compare.cpp
+++ mpi_sum_compare.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <vector>
 #include <numeric>
+#include <cstdlib>
 
 int main(int argc, char** argv) {
     MPI_Init(&argc, &argv);
@@ -10,55 +11,81 @@ int main(int argc, char** argv) {
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-    int local_value = rank + 1; // Each rank contributes its rank + 1
-    int result_accumulate = 0;
-    int result_get = 0;
-
-    MPI_Win win;
-    MPI_Win_create(&result_accumulate, sizeof(int), sizeof(int), MPI_INFO_NULL, MPI_COMM_WORLD, &win);
-
-    // --- MPI_Accumulate ---
-    MPI_Win_fence(0, win);
-    double start_acc = MPI_Wtime();
-    if (rank != 0) {
-        MPI_Accumulate(&local_value, 1, MPI_INT, 0, 0, 1, MPI_INT, MPI_SUM, win);
-    }
-    MPI_Win_fence(0, win);
-    double end_acc = MPI_Wtime();
-
-    if (rank == 0) {
-        result_accumulate += local_value; // Add own value
-        std::cout << "MPI_Accumulate result: " << result_accumulate
-                  << ", Time: " << (end_acc - start_acc) << " seconds\n";
-    }
-
-    // --- MPI_Get ---
-    std::vector<int> all_values(size, 0);
-    MPI_Win win_get;
-    MPI_Win_create(&local_value, sizeof(int), sizeof(int), MPI_INFO_NULL, MPI_COMM_WORLD, &win_get);
-    MPI_Win_fence(0, win_get);
-
-    double start_get = MPI_Wtime();
-    if (rank == 0) {
-        for (int i = 0; i < size; ++i) {
-            MPI_Get(&all_values[i], 1, MPI_INT, i, 0, 1, MPI_INT, win_get);
+    if (argc < 2) {
+        if (rank == 0) {
+            std::cerr << "Usage: " << argv[0] << " <size1> <size2> ...\n";
         }
+        MPI_Finalize();
+        return 1;
     }
-    MPI_Win_fence(0, win_get);
-    double end_get = MPI_Wtime();
 
     if (rank == 0) {
-        result_get = std::accumulate(all_values.begin(), all_values.end(), 0);
-        std::cout << "MPI_Get result: " << result_get
-                  << ", Time: " << (end_get - start_get) << " seconds\n";
+        std::cout << "method,data_size,time_sec,result_sum\n";
     }
 
-    MPI_Win_free(&win);
-    MPI_Win_free(&win_get);
+    for (int i = 1; i < argc; ++i) {
+
+        size_t data_size = std::stoul(argv[i]);
+
+        std::vector<double> local_data(data_size, rank + 1.0);
+        std::vector<double> result_accumulate(data_size, 0.0);
+        std::vector<double> result_get(size, 0.0);  // one value per rank
+
+        // --- MPI_Accumulate ---
+        MPI_Win win_acc;
+        MPI_Win_create(result_accumulate.data(), data_size * sizeof(double), sizeof(double),
+                       MPI_INFO_NULL, MPI_COMM_WORLD, &win_acc);
+
+        MPI_Barrier(MPI_COMM_WORLD);
+        double start_acc = MPI_Wtime();
+
+        MPI_Win_fence(0, win_acc);
+        if (rank != 0) {
+            MPI_Accumulate(local_data.data(), data_size, MPI_DOUBLE,
+                           0, 0, data_size, MPI_DOUBLE, MPI_SUM, win_acc);
+        }
+        MPI_Win_fence(0, win_acc);
+        double end_acc = MPI_Wtime();
+
+        if (rank == 0) {
+            for (size_t j = 0; j < data_size; ++j) {
+                result_accumulate[j] += local_data[j];  // add own value
+            }
+            double sum = std::accumulate(result_accumulate.begin(), result_accumulate.end(), 0.0);
+            std::cout << "accumulate," << data_size << "," << (end_acc - start_acc) << "," << sum << "\n";
+        }
+
+        MPI_Win_free(&win_acc);
+
+        // --- MPI_Get ---
+        MPI_Win win_get;
+        MPI_Win_create(local_data.data(), data_size * sizeof(double), sizeof(double),
+                       MPI_INFO_NULL, MPI_COMM_WORLD, &win_get);
+        MPI_Barrier(MPI_COMM_WORLD);
+
+        double start_get = MPI_Wtime();
+        std::vector<double> gathered(data_size * size, 0.0);
+        MPI_Win_fence(0, win_get);
+        if (rank == 0) {
+            for (int r = 0; r < size; ++r) {
+                MPI_Get(&gathered[r * data_size], data_size, MPI_DOUBLE,
+                        r, 0, data_size, MPI_DOUBLE, win_get);
+            }
+        }
+        MPI_Win_fence(0, win_get);
+        double end_get = MPI_Wtime();
+
+        if (rank == 0) {
+            double sum = std::accumulate(gathered.begin(), gathered.end(), 0.0);
+            std::cout << "get," << data_size << "," << (end_get - start_get) << "," << sum << "\n";
+        }
+
+        MPI_Win_free(&win_get);
+    }
+
     MPI_Finalize();
 
     return 0;
 
 }
-
 
